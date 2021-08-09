4d353c543a62a73ea1b85f39873a31db1c7eb91e
Peter Moser <p.moser@noi.bz.it>
2021-08-09 21:26:32 +0200

    infra: fix CI pipeline + update dependencies

--- infrastructure/jenkins/ci.groovy
+++ infrastructure/jenkins/ci.groovy
@@ -1,26 +1,39 @@
 pipeline {
-    agent {
-        dockerfile {
-            filename 'infrastructure/docker/7.4/Dockerfile'
-            additionalBuildArgs '--build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins) --build-arg WWWGROUP=$(id -g jenkins)'
-        }
+    options {
+        ansiColor('xterm')
     }
+	agent {
 
-    stages {
-        stage('Backend - Test') {
-            steps {
-                sh '''
-                   php artisan test
-                '''
-            }
-        }
-        stage('Frontend - Test') {
-            steps {
-                sh '''
-                    npm install
-                    npm run prod
-                '''
-            }
-        }
-    }
+		// FIXME The container started but didn't run the expected command.
+		// Please double check your ENTRYPOINT does execute the command passed
+		// as docker run argument, as required by official docker images (see
+		// https://github.com/docker-library/official-images#consistency for
+		// entrypoint consistency requirements). Alternatively you can force
+		// image entrypoint to be disabled by adding option `--entrypoint=''`.
+		dockerfile {
+			filename 'infrastructure/docker/7.4/Dockerfile'
+			additionalBuildArgs '--target jenkinsci --build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)'
+			args '--entrypoint=""'
+		}
+	}
+	stages {
+		stage('Backend - Test') {
+			steps {
+				sh """
+					cp .env.example .env
+					php artisan key:generate
+					composer install
+					php artisan test --without-tty
+				"""
+			}
+		}
+		stage('Frontend - Test') {
+			steps {
+				sh '''
+					npm install
+					npm run prod
+				'''
+			}
+		}
+	}
 }
