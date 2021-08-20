23f2d87c80ff728c07cb9f884267f8e702367626
Alex C <alex@agon-e.com>
2021-08-20 08:33:22 +0200

    force https in production environments

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+		// https://stackoverflow.com/questions/34378122/load-blade-assets-with-https-in-laravel/45731506#45731506
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
