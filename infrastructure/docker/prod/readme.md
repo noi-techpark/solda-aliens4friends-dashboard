34b0a430af4075713fe6352c4f1c09f8b01f8c18
Peter Moser <p.moser@noi.bz.it>
2021-10-15 14:57:06 +0200

    Revert "Docker improvements for production use (#24)"
    This reverts commit 76d0dd6aaf481006b91278ce212bf160e797a73c.

---
+++ infrastructure/docker/prod/readme.md
@@ -0,0 +1,3 @@
+Taken from https://www.koyeb.com/tutorials/dockerize-and-deploy-a-laravel-application-to-production
+
+not tested, example production container
--- infrastructure/jenkins/ci.groovy
+++ infrastructure/jenkins/ci.groovy
@@ -20,7 +20,7 @@ pipeline {
 		stage('Backend - Test') {
 			steps {
 				sh """
-					cp .env.default .env
+					cp .env.example .env
 					composer install
 					php artisan key:generate
 					php artisan test --without-tty
