ad998dcf0a8683b10bf33b3c3e44676d6a150fed
Martin Rabanser <martin.rabanser@rmb-consulting.tech>
2021-11-09 11:02:06 +0000

    Updated logos and partners

--- resources/js/presets/partner.js
+++ resources/js/presets/partner.js
@@ -1,19 +1,27 @@
 exports.companies = [
 	{
-		name : "HUAWEI TECHNOLOGIES",
-		title : "HUAWEI TECHNOLOGIES",
-		alt: "HUAWAI",
+		name : "ONIRO",
+		title : "ONIRO",
+		alt: "ONIRO",
 		size: "4",
-		logo_url: "https://www-file.huawei.com/-/media/corporate/images/home/logo/huawei_logo.png",
-		link: "https://www.huawei.com/"
+		logo_url: "img/ONIRO-Masterbrand-vertical.svg",
+		link: "https://oniroproject.org/"
 	},
 	{
-		name : "NOI Techpark Südtirol / Alto Adige",
-		title : "NOI Techpark Südtirol / Alto Adige",
-		alt: "NOI",
+		name : "Array",
+		title : "Array",
+		alt: "Array",
 		size: "4",
-		logo_url: "https://noi.bz.it/themes/custom/noi_techpark/logo_bn.svg",
-		link: "https://noi.bz.it/"
+		logo_url: "img/logo_array.svg",
+		link: "https://www.array.eu/"
+	},
+	{
+		name : "NOI Free Software Lab",
+		title : "NOI Free Software Lab",
+		alt: "NOI Free Software Lab",
+		size: "4",
+		logo_url: "img/logo_NOI_bn.svg",
+		link: "https://freesoftwarelab.noi.bz.it"
 	},
 	{
 		name : "Europäischer Fonds für regionale Entwicklung",
@@ -21,55 +29,48 @@ exports.companies = [
 		alt: "EFRE",
 		size: "4",
 		logo_url: "https://noi.bz.it/sites/default/files/inline-images/efre-fesr.jpg",
-		link: "hhttps://noi.bz.it/"
-	},
-	{
-		name : "HUAWEI TECHNOLOGIES",
-		title : "",
-		alt: "HUAWAI",
-		size: "2",
-		logo_url: "https://www-file.huawei.com/-/media/corporate/images/home/logo/huawei_logo.png",
-		link: "https://www.huawei.com/"
-	},
-	{
-		name : "NOI Techpark Südtirol / Alto Adige",
-		title : "",
-		alt: "NOI",
-		size: "2",
-		logo_url: "https://noi.bz.it/themes/custom/noi_techpark/logo_bn.svg",
-		link: "hhttps://noi.bz.it/"
-	},
-	{
-		name : "Europäischer Fonds für regionale Entwicklung",
-		title : "",
-		alt: "EFRE",
-		size: "2",
-		logo_url: "https://noi.bz.it/sites/default/files/inline-images/efre-fesr.jpg",
-		link: "hhttps://noi.bz.it/"
-	},
-	{
-		name : "NOI Techpark Südtirol / Alto Adige",
-		title : "",
-		alt: "NOI",
-		size: "2",
-		logo_url: "https://noi.bz.it/themes/custom/noi_techpark/logo_bn.svg",
-		link: "hhttps://noi.bz.it/"
-	},
-	{
-		name : "Europäischer Fonds für regionale Entwicklung",
-		title : "",
-		alt: "EFRE",
-		size: "2",
-		logo_url: "https://noi.bz.it/sites/default/files/inline-images/efre-fesr.jpg",
-		link: "hhttps://noi.bz.it/"
-	},
-	{
-		name : "HUAWEI TECHNOLOGIES",
-		title : "",
-		alt: "HUAWAI",
-		size: "2",
-		logo_url: "https://www-file.huawei.com/-/media/corporate/images/home/logo/huawei_logo.png",
-		link: "https://www.huawei.com/"
+		link: "https://noi.bz.it/"
 	},
 
+/*	{
+		name : "NOI Techpark Südtirol / Alto Adige",
+		title : "",
+		alt: "NOI",
+		size: "2",
+		logo_url: "https://noi.bz.it/themes/custom/noi_techpark/logo_bn.svg",
+		link: "hhttps://noi.bz.it/"
+	},
+	{
+		name : "Europäischer Fonds für regionale Entwicklung",
+		title : "",
+		alt: "EFRE",
+		size: "2",
+		logo_url: "https://noi.bz.it/sites/default/files/inline-images/efre-fesr.jpg",
+		link: "hhttps://noi.bz.it/"
+	},
+	{
+		name : "NOI Techpark Südtirol / Alto Adige",
+		title : "",
+		alt: "NOI",
+		size: "2",
+		logo_url: "https://noi.bz.it/themes/custom/noi_techpark/logo_bn.svg",
+		link: "hhttps://noi.bz.it/"
+	},
+	{
+		name : "Europäischer Fonds für regionale Entwicklung",
+		title : "",
+		alt: "EFRE",
+		size: "2",
+		logo_url: "https://noi.bz.it/sites/default/files/inline-images/efre-fesr.jpg",
+		link: "hhttps://noi.bz.it/"
+	},
+	{
+		name : "HUAWEI TECHNOLOGIES",
+		title : "",
+		alt: "HUAWAI",
+		size: "2",
+		logo_url: "https://www-file.huawei.com/-/media/corporate/images/home/logo/huawei_logo.png",
+		link: "https://www.huawei.com/"
+	},
+*/
 ]
